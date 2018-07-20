c078b49a6f4a65f7a5302401394ff5874d401130
hatlonely <hatlonely@gmail.com>
2018-07-20 13:10:20 +0800

    update

--- src/thrift_0_11_0_echo/client/main.cpp
+++ src/thrift_0_11_0_echo/client/main.cpp
@@ -1,20 +1,62 @@
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/stdcxx.h>
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TTransportUtils.h>
+#include <unistd.h>
 #include <iostream>
 #include <thread>
 #include <vector>
-#include <unistd.h>
+#include "Service.h"
 
-int main(int argc, const char *argv[]) {
-    std::cout << "hello world" << std::endl;
-    
+int nowUs() {
+    timeval now = {0, 0};
+    gettimeofday(&now, NULL);
+
+    return int(now.tv_sec * 1000000 + now.tv_usec);
+}
+
+int main(int argc, const char* argv[]) {
+    int threadNum = 1;
+    int requestNum = 100;
+
     std::vector<std::thread*> vt;
-    for (int i = 0; i < 10; i++) {
-        vt.emplace_back(new std::thread([&](){
-            std::cout << "hello world 666" << std::endl;
-        }));
+    std::vector<int> times(threadNum);
+    for (int i = 0; i < threadNum; i++) {
+        vt.emplace_back(new std::thread([&](int idx) {
+            for (int j = 0; j < requestNum; j++) {
+                auto t1 = nowUs();
+                auto socket = apache::thrift::stdcxx::make_shared<apache::thrift::transport::TSocket>("127.0.0.1", 9090);
+                auto transport = apache::thrift::stdcxx::make_shared<apache::thrift::transport::TBufferedTransport>(socket);
+                auto protocol = apache::thrift::stdcxx::make_shared<apache::thrift::protocol::TBinaryProtocol>(transport);
+                addservice::ServiceClient client(protocol);
+
+                try {
+                    transport->open();
+
+                    addservice::Response response;
+                    addservice::Request request;
+                    request.a = 1;
+                    request.b = 2;
+                    client.add(response, request);
+
+                    transport->close();
+                } catch (apache::thrift::TException& tx) {
+                    std::cout << "ERROR: " << tx.what() << std::endl;
+                }
+                auto t2 = nowUs();
+                times[idx] += t2 - t1;
+            }
+        }, i));
     }
-    for (auto t: vt) {
+    for (auto t : vt) {
         if (t->joinable()) {
             t->join();
         }
     }
+    int totalTime = 0;
+    for (auto i : times) {
+        totalTime += i;
+    }
+
+    std::cout << totalTime / threadNum / requestNum << std::endl;
 }
