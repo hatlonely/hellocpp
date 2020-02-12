906adf19f813fa65f6b5ad70114b46d9d25b3ff4
hatlonely <hatlonely@gmail.com>
2020-02-13 01:48:52 +0800

    update

--- src/std/test_set.cpp
+++ src/std/test_set.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include <set>
+#include <unordered_set>
 
 TEST(testSet, case1) {
     {
@@ -28,5 +29,43 @@ TEST(testSet, case1) {
         auto p = si.equal_range(6);
         EXPECT_EQ(*p.first, 6);   // >= 6
         EXPECT_EQ(*p.second, 7);  // > 6
+    }
+}
+
+TEST(testMultiSet, case1) {
+    {
+        std::multiset<int> si = {1, 1, 1, 2, 3, 3, 4, 4, 5};
+        EXPECT_FALSE(si.empty());
+        EXPECT_EQ(si.size(), 9);
+        EXPECT_EQ(si.count(1), 3);
+
+        si.insert(1);
+        EXPECT_EQ(si.count(1), 4);
+        si.erase(1);
+        EXPECT_EQ(si.count(1), 0);
+    }
+}
+
+TEST(testUnorderedSet, case1) {
+    {
+        std::unordered_set<int> si = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        EXPECT_FALSE(si.empty());
+        EXPECT_EQ(si.size(), 9);
+
+        EXPECT_EQ(si.count(0), 0);
+        EXPECT_EQ(si.find(0), si.end());
+    }
+    {
+        std::unordered_set<int> si = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        si.insert(0);
+        EXPECT_EQ(si, std::unordered_set<int>({1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
+        si.erase(0);
+        EXPECT_EQ(si, std::unordered_set<int>({1, 2, 3, 4, 5, 6, 7, 8, 9}));
+    }
+    {
+        std::unordered_set<int> si = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        si.rehash(100);
+        EXPECT_NEAR(si.load_factor(), 0.1, 0.001);
+        EXPECT_EQ(si.max_load_factor(), 1);
     }
 }
