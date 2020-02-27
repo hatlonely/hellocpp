3af6bf2a1a039eb0a1b6a2410e2a64d556d4e8f5
hatlonely <hatlonely@gmail.com>
2020-02-27 08:16:27 +0800

    update

--- src/std/test_io.cpp
+++ src/std/test_io.cpp
@@ -1,6 +1,24 @@
 #include <gtest/gtest.h>
+#include <fstream>
 #include <iostream>
 
-TEST(testIO, case1) {
+TEST(testIO, caseFile) {
+    {
+        std::ofstream fout("/tmp/test.txt");
+        fout << "做一个决定，并不难，难的是付诸行动，并且坚持到底";
+        fout.flush();
+        fout.close();
+    }
+    {
+        std::ifstream fin("/tmp/test.txt");
+        std::string   str;
+        fin >> str;
+        EXPECT_EQ(str, "做一个决定，并不难，难的是付诸行动，并且坚持到底");
+    }
+}
 
+TEST(testIO, caseString) {
+    std::stringstream ss;
+    ss << "pi = " << 3.14;
+    EXPECT_EQ(ss.str(), "pi = 3.14");
 }
--- src/std/test_thread.cpp
+++ src/std/test_thread.cpp
@@ -1,5 +1,36 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <thread>
+
+struct add_function_t {
+    void operator()(int a, int b, int &result) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(60));
+        result = a + b;
+        std::cout << "add_function_t: " << a << " + " << b << " = " << result << std::endl;
+    }
+};
 
 TEST(testThread, case1) {
+    int a = 1, b = 2, result1, result2;
+
+    std::thread t1([]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        std::cout << "t1" << std::endl;
+    });
+
+    std::thread t2([](int a, int b, int &result) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(40));
+        result = a + b;
+        std::cout << "add: " << a << " + " << b << " = " << result << std::endl;
+    },
+                   a, b, std::ref(result1));
+
+    std::thread t3(add_function_t(), a, b, std::ref(result2));
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    EXPECT_EQ(result1, 3);
+    EXPECT_EQ(result2, 3);
 }
