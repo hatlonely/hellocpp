f6dac32784067db1fc31314b5d087e34962f0140
hatlonely <hatlonely@gmail.com>
2020-03-01 05:41:37 +0800

    update

--- src/boost/test_process.cpp
+++ src/boost/test_process.cpp
@@ -1,5 +1,18 @@
 #include <gtest/gtest.h>
+#include <boost/process.hpp>
 #include <iostream>
 
 TEST(testProcess, case1) {
+    boost::process::ipstream out;
+    boost::process::child    c("gcc --version", boost::process::std_out > out);
+
+    if (!c.wait_for(std::chrono::seconds(10))) {
+        c.terminate();
+    }
+
+    while (out) {
+        std::string line;
+        std::getline(out, line);
+        std::cout << line << std::endl;
+    }
 }
