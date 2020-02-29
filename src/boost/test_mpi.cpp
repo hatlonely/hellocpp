8b6cb9e4b65065a6e0f9d0edd06ace697cd28774
hatlonely <hatlonely@gmail.com>
2020-03-01 05:20:51 +0800

    update

--- src/boost/test_mpi.cpp
+++ src/boost/test_mpi.cpp
@@ -1,6 +1,25 @@
-#include <gtest/gtest.h>
-#include <iostream>
-
-TEST(testMPI, case1) {
-    
-}
+//#include <gtest/gtest.h>
+//#include <iostream>
+//#include <thread>
+//#include <vector>
+//#include <mpi.h>
+//
+//void processor() {
+//    int argc;
+//    const char* argv;
+//    MPI_Init(nullptr, nullptr);
+//
+//    int rank;
+//    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//
+//    MPI_Finalize();
+//}
+//
+//TEST(testMPI, case1) {
+//    std::vector<std::thread> ps(10);
+//
+//    auto now = std::chrono::system_clock::now();
+//    for (int i = 0; i < ps.size(); i++) {
+//        ps[i] = std::thread(processor);
+//    }
+//}
