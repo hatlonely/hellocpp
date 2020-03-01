3976107dcdc8345697b6c0c135741034ae45f974
hatlonely <hatlonely@gmail.com>
2020-03-01 10:33:23 +0800

    update

--- src/boost/test_static_assert.cpp
+++ src/boost/test_static_assert.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <boost/static_assert.hpp>
 
 TEST(testStaticAssert, case1) {
+    BOOST_STATIC_ASSERT(true);
+    BOOST_STATIC_ASSERT_MSG(true, "fail message");
 }
