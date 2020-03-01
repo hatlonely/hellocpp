a95fa4fd512bc6ee63c2ff41d26f5efaf749d54e
hatlonely <hatlonely@gmail.com>
2020-03-01 10:58:08 +0800

    update

--- src/boost/test_typeindex.cpp
+++ src/boost/test_typeindex.cpp
@@ -1,5 +1,29 @@
 #include <gtest/gtest.h>
+#include <boost/type_index.hpp>
 #include <iostream>
 
+namespace test {
+namespace tidx {
+
+class A {};
+
+}
+}
+
 TEST(testTypeIndex, case1) {
+    std::cout << typeid(test::tidx::A).name() << std::endl;
+    std::cout << boost::typeindex::type_id<test::tidx::A>().name() << std::endl;
+    std::cout << boost::typeindex::type_id<test::tidx::A>().pretty_name() << std::endl;
+
+    EXPECT_EQ(typeid(test::tidx::A).name(), boost::typeindex::type_id<test::tidx::A>().name());
+    EXPECT_EQ(boost::typeindex::type_id<test::tidx::A>().pretty_name(), "test::tidx::A");
+
+    std::cout << typeid(const test::tidx::A).name() << std::endl;
+    std::cout << boost::typeindex::type_id<const test::tidx::A>().name() << std::endl;
+    std::cout << boost::typeindex::type_id_with_cvr<const test::tidx::A>().name() << std::endl;
+    std::cout << boost::typeindex::type_id_with_cvr<const test::tidx::A>().pretty_name() << std::endl;
+
+    EXPECT_EQ(typeid(const test::tidx::A).name(), boost::typeindex::type_id<const test::tidx::A>().name());
+    EXPECT_NE(typeid(const test::tidx::A).name(), boost::typeindex::type_id_with_cvr<const test::tidx::A>().name());
+    EXPECT_EQ(boost::typeindex::type_id_with_cvr<const test::tidx::A>().pretty_name(), "test::tidx::A const");
 }
